be863af904892bc7441002be1e9628987549c238
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2021-02-01 23:27:28 +0100

    [ADV-2] feat: update DQN model code and fix all issues

--- FlappyBird/models/dqn.py
+++ FlappyBird/models/dqn.py
@@ -1,5 +1,8 @@
+import torch
 from torch import nn
 from torch.nn.modules import Module
+
+import numpy as np
 
 
 class DQNModule(Module):
@@ -7,30 +10,33 @@ class DQNModule(Module):
         super().__init__()
         self.img_w = img_w
         self.img_h = img_h
+
         self.conv_network = nn.Sequential(
-            nn.Conv2d(n_frames, 64,kernel_size=3),  # input => 80 x 80 x 4
-            nn.BatchNorm2d(32),
+            nn.Conv2d(n_frames, 32, kernel_size=3),  # input => 80 x 80 x 4
             nn.ReLU(),
-            nn.MaxPool2d((2,2)),  # output = 40 x 40 x 32
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d((2, 2)),  # output = 40 x 40 x 32
 
             nn.Conv2d(32, 64, kernel_size=3),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d((2,2)),  # output = 20 x 20 x 64
+            # nn.BatchNorm2d(64),
+            nn.MaxPool2d((2, 2)),  # output = 20 x 20 x 64
 
             nn.Conv2d(64, 64, kernel_size=3),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d((2,2)),  # output = 10 x 10 x 64
+            # nn.BatchNorm2d(64),
+            nn.MaxPool2d((2, 2)),  # output = 10 x 10 x 64
 
             nn.Conv2d(64, 32, kernel_size=3),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d((2,2)),  # output = 5 x 5 x 32
+            # nn.BatchNorm2d(32),
+            nn.MaxPool2d((2, 2)),  # output = 5 x 5 x 32
         )
 
+        self.linear_input_size = self.get_linear_input_size(n_frames)
+
         self.linear_network = nn.Sequential(
-            nn.Linear(5*5*32, 128),
+            nn.Linear(self.linear_input_size, 128),
             nn.ReLU(),
 
             nn.Linear(128, 32),
@@ -41,7 +47,24 @@ class DQNModule(Module):
 
     def forward(self, x):
         x = self.conv_network(x)
-        x = x.view(-1, 5*5*32)
+        x = x.view(-1, self.flatten_shape(x))
         x = self.linear_network(x)
 
         return x
+
+    def get_linear_input_size(self, n_frames: int):
+        x = np.zeros((1, n_frames, self.img_w, self.img_h))
+        x = torch.from_numpy(x)
+        y = self.conv_network(x.float())
+
+        return self.flatten_shape(y)
+
+    @staticmethod
+    def flatten_shape(x: torch.Tensor):
+        dims = x.size()[1:]
+        flat_shape = 1
+        for dim in dims:
+            flat_shape *= dim
+
+        return flat_shape
+
